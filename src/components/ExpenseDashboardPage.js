f09e6353da4a7fdc6a571503bf4fb54342d34b0a
Jillo Woche <jillo.abdulla@gmail.com>
2019-02-06 16:46:11 +0300

    add a help page

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -4,6 +4,11 @@ import ExpenseListFilters from './ExpenseListFilters';
 
 const ExpenseDashboardPage = () => (
     <div>
+    <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Dashboard</h1>
+          </div>
+        </div>
         <ExpenseListFilters />
         <ExpenseList />
     </div>
--- src/components/HelpPage.js
+++ src/components/HelpPage.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 const HelpPage = () => (
     <div>
-        This is from help page
+    <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Expensify is an app that helps track your expenses.</h1>
+          </div>
+        </div>
+        <div className="content-container">
+            <div>
+            <ol className="help-list">
+                <li><span className="list-number">1.</span><p>Click on <i>Add Expense</i> above to add a new expense.</p></li>
+                <li><span className="list-number"> 2.</span><p>View your added expenses on the <i>Dashboard</i> after adding an expense.</p></li>
+                <li><span className="list-number">3.</span><p>You can filter your expenses using date or amount from the dashboard.</p></li>
+            </ol>
+        </div>
+        </div>
     </div>
 );
 
